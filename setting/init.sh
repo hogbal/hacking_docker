aebd80745f742be626d5b6e0742ab40a097898ea
hogbal <hogbal31@gmail.com>
2023-05-23 12:09:19 +0900

    init.sh

--- setting/init.sh
+++ setting/init.sh
@@ -8,8 +8,8 @@ cp vimrc ~/.vimrc
 # setting tmux
 echo "Setting tmux"
 
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f ~/.tmux/.tmux.conf
+git clone https://github.com/gpakosz/.tmux.git ~/
+ln -s -f ~/.tmux/.tmux.conf ~/
 cp ~/.tmux/.tmux.conf.local ~/
 
 # setting oh my zsh
@@ -20,7 +20,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-autosuggestions  ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
 
-echo './powerlevel10k.zsh-theme' >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
 echo "export LC_ALL=en_US.UTF-8" >> ~/.zshrc
 echo "export PYTHONIOENCODING=UTF-8" >> ~/.zshrc
 echo "POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true" >> ~/.zshrc
