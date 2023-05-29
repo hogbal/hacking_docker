f322678c9a98686d31067b65f5d95bb4df32d0e6
hogbal <hogbal31@gmail.com>
2023-05-29 11:48:58 +0900

    ADD server/request

--- server/request/app.py
+++ server/request/app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -5,19 +6,27 @@ app = Flask(__name__)
 request_data = []
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/<path:subpath>", methods=["GET", "POST"])
+def index(subpath=None):
 	request_dict = {}
+	request_dict["idx"] = len(request_data)
 	request_dict["remote_addr"] = request.remote_addr
 	request_dict["method"] = request.method
 	request_dict["path"] = request.path
 	request_dict["query_string"] = request.query_string
 	
-	request_dict["headers"] = request.headers
+	request_header = ""
+	for key, value in request.headers:
+		request_header += f"{key} : {value}<br>"
+	
+	request_dict["headers"] = request_header
 	request_dict["values"] = request.values.to_dict()
+	request_dict["time"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	request_data.append(request_dict)
 	return "success"
 
 @app.route("/request_list")
 def request_list():
-	return render_template("index.html")
+	return render_template("index.html", request_data=request_data)
 
 app.run(host="0.0.0.0", port=5000)
