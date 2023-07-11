963e9924868ae6d104ff291619a10e64403ba0b1
hogbal <hogbal31@gmail.com>
2023-07-11 04:06:17 -0700

    Update setting/init.sh

--- setting/init.sh
+++ setting/init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+sudo apt-get install vim tmux zsh -y
+
 # setting vim
 echo "Setting vim"
 
@@ -8,7 +11,7 @@ cp vimrc ~/.vimrc
 # setting tmux
 echo "Setting tmux"
 
-git clone https://github.com/gpakosz/.tmux.git ~/
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf ~/
 cp ~/.tmux/.tmux.conf.local ~/
 
@@ -16,13 +19,17 @@ cp ~/.tmux/.tmux.conf.local ~/
 echo "Setting oh my zsh"
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions  ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
+chsh -s `which zsh`
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/tools/powerlevel10k
-echo 'source ~/tools/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+# setting fonts
+echo "Setting Fonts"
+git clone https://github.com/powerline/fonts.git
+cd fonts && ./install.sh
+cd .. && rm -rf fonts
 
 echo "export LC_ALL=en_US.UTF-8" >> ~/.zshrc
 echo "export PYTHONIOENCODING=UTF-8" >> ~/.zshrc
-echo "POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true" >> ~/.zshrc
